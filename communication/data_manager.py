251539a9c6ce85c1f60dcdc4831c170fd88aa930
Antoine <36314396+antoinepetty@users.noreply.github.com>
2019-03-11 18:16:08 +0000

    Added JSON keys associated with each Arduino
    Please refer to this: https://docs.google.com/spreadsheets/d/1Fdt1rVjEnA7RdnwTZAiLM3k-1iecr5KCTRdCzaJO3LI/edit#gid=0

--- communication/data_manager.py
+++ communication/data_manager.py
@@ -42,10 +42,10 @@ Kacper Florianski
 
 # Declare constants to easily access the resources
 SURFACE = 0
-ARDUINO_A = "Ard-T"
-ARDUINO_B = 2
-ARDUINO_C = 3
-ARDUINO_D = 4
+ARDUINO_T = "Ard-T"
+ARDUINO_A = "Ard-A"
+ARDUINO_M = "Ard-M"
+ARDUINO_I = "Ard-I"
 
 
 class DataManager:
@@ -54,27 +54,27 @@ class DataManager:
 
         # Declare dictionaries of data
         self._surface = dict()
+        self._arduino_T = dict()
         self._arduino_A = dict()
-        self._arduino_B = dict()
-        self._arduino_C = dict()
-        self._arduino_D = dict()
+        self._arduino_M = dict()
+        self._arduino_I = dict()
 
         # Create a dictionary mapping each index to corresponding location
         self._data = {
             SURFACE: self._surface,
+            ARDUINO_T: self._arduino_T,
             ARDUINO_A: self._arduino_A,
-            ARDUINO_B: self._arduino_B,
-            ARDUINO_C: self._arduino_C,
-            ARDUINO_D: self._arduino_D
+            ARDUINO_M: self._arduino_M,
+            ARDUINO_I: self._arduino_I
         }
 
         # Create a dictionary mapping each index to a set of networking keys
         self._transmission_keys = {
             SURFACE: {"example"},
-            ARDUINO_A: {"lay", "lax"},
-            ARDUINO_B: {},
-            ARDUINO_C: {},
-            ARDUINO_D: {}
+            ARDUINO_T: {"Thr-FP", "Thr-FS", "Thr-AP", "Thr-AS", "Thr-TFP", "Thr-TFS", "Thr-TAP", "Thr-TAS"},
+            ARDUINO_A: {"Mot-R", "Mot-G", "Mot-F"},
+            ARDUINO_M: {"Thr-M", "LED-M"},
+            ARDUINO_I: {"Sen-IMU", "Sen-Dep", "Sen-Temp", "Sen-Leak"}
         }
 
     def get(self, index: int, *args, transmit=False):
