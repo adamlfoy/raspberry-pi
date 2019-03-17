aed11a2934b1a128f0c544d1400e1e3b7ec21794
Kacper Florianski <36332694+thecodesummoner@users.noreply.github.com>
2019-03-17 16:37:22 +0000

    Added test scripts

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 # TODO: Fix relative path issues on PI (after adding the server to run on boot)
-
+# TODO: Remove unused imports
 import communication.data_manager as dm
 from communication.server import Server
+from communication.video_stream import VideoStream
+from cv2 import VideoCapture
+
+
+# TODO: Remove this test script
+def sample_video_stream():
+    cap = VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        video_stream.frame = frame
+
+
+# TODO: Remove this test script
+def sample_text_debug():
+    while True:
+        print(dm.get_data(dm.SURFACE), dm.get_data(dm.ARDUINO_A), dm.get_data(dm.ARDUINO_I), dm.get_data(dm.ARDUINO_M))
 
 
 if __name__ == "__main__":
@@ -10,7 +26,14 @@ if __name__ == "__main__":
     dm.clear()
 
     # Initialise the server
-    s = Server()
+    server = Server()
+
+    # Initialise the video stream
+    video_stream = VideoStream()
 
     # Start the tasks
-    s.run()
+    server.run()
+    video_stream.run()
+
+    # TODO: Remove this test script
+    sample_video_stream()
