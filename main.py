032c2d0e9c19accf549c21fe3bb0d37e7a39ac10
Kacper Florianski <36332694+thecodesummoner@users.noreply.github.com>
2019-04-01 17:51:50 +0200

    Adjusted main for easier debug

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-# TODO: Fix relative path issues on PI (after adding the server to run on boot)
-# TODO: Remove unused imports
 import communication.data_manager as dm
 from communication.server import Server
 from communication.video_stream import VideoStream
 from cv2 import VideoCapture
+from time import sleep
 
 
 # TODO: Remove this test script
-def sample_video_stream():
-    cap = VideoCapture(0)
+def blocking_test_video_stream(streams):
+    caps = []
+    for iterator, stream in enumerate(streams):
+        caps.append(VideoCapture(iterator))
     while True:
-        ret, frame = cap.read()
-        video_stream.frame = frame
+        for iterator, stream in enumerate(streams):
+            ret, frame = caps[iterator].read()
+            stream.frame = frame
 
 
 # TODO: Remove this test script
-def sample_text_debug():
+def blocking_test_text_debug():
     while True:
         print(dm.get_data(dm.SURFACE), dm.get_data(dm.ARDUINO_A), dm.get_data(dm.ARDUINO_I), dm.get_data(dm.ARDUINO_M))
+        sleep(0.5)
 
 
 if __name__ == "__main__":
@@ -28,12 +31,11 @@ if __name__ == "__main__":
     # Initialise the server
     server = Server()
 
-    # Initialise the video stream
+    # Initialise the video streams
     video_stream = VideoStream()
+    vs = VideoStream(port=50002)
 
     # Start the tasks
     server.run()
     video_stream.run()
-
-    # TODO: Remove this test script
-    sample_video_stream()
+    vs.run()
